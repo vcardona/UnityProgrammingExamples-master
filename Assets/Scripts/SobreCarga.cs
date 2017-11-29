e6f731c8209330c185b12aa2e3b07c52eca87a9a
Victor Cardona <victorhugocardonagomez@gmail.com>
2017-11-29 15:38:28 -0500

    Ejemplo GitHub
    Se creo un nuevo metodo para mostrar los cambios en GitHub Desktop

--- Assets/Scripts/SobreCarga.cs
+++ Assets/Scripts/SobreCarga.cs
@@ -7,10 +7,16 @@ public class SobreCarga : MonoBehaviour
 	public int Suma(int numeroUno, int numeroDos)
     {
         return numeroUno + numeroDos;
+        
     }
 
     public string Suma(string letraUno, string letraDos)
     {
         return letraUno + letraDos;
+    }
+
+    public void saludos()
+    {
+        Debug.Log("Muestra cambios GitHub");
     }
 }
